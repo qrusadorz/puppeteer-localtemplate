d032fb87014eb46779157e22d9559225cbbbeec8
qrusadorz <qrusadorz@gmail.com>
2019-04-04 22:13:03 +0900

    add comment

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ const crawlSites = async (sites, browser) => {
         console.error(e);
     } finally {
         // 規定時間未満の連続アクセス防止
-        const processTime = (Date.now() - nowTime); //  / 1000;
+        const processTime = (Date.now() - nowTime);
         if (processTime < crawlDuration) {
             console.log('sleep...');
             await sleep(crawlDuration - processTime);
@@ -216,7 +216,7 @@ const crawlMain = async (browser, items, result, resultFailed) => {
     }
 };
 
-(async () => {
+(async () => {    
     let browser = { close: () => { } };
     try {
         browser = await puppeteer.launch({
@@ -246,7 +246,41 @@ const crawlMain = async (browser, items, result, resultFailed) => {
         const resultFailed = { items: [], timestamp };
 
         // TODO 差分更新に対応する場合、効率を考えると一旦オブジェクトに変換したいところ。
-        // 数が少ないのでfind活用でもいいかもしれないが…find->remove
+        // 差分更新のシナリオ
+        // 1.1  recoveryロジックを利用して、latest.json(Array)をロード
+        //        result.items = readItems().items;
+        // 1.2  1.1をObjectに変換
+        //        // Array => Object
+        //        params.forEach(value => result[value.key?] = value);
+        // 2.   差分Items(restore同様実質Idsになるだろう)のロード＜＝高頻度抽出は別途検討
+        //        readFailedItems().itemsレベルもの
+        // 3.   2の差分高頻度更新データをcrawl
+        //        await crawlMain(browser, getSelectItems(readFailedItems().items), result, resultFailed);
+        // 4.1. 3の結果をObjectに変換
+        //        // Array => Object
+        //        const diffItems = result.items.forEach(value => resultDiff[value.key?] = value);
+        // 4.2. 4.1の結果を1.2に上書き（取得分のみ更新）
+        //        diffItems.forEach(value => result[value.key?] = value);
+        // 5.1. 4.2の結果をArrayに変換
+        //        // Object => Array
+        //        const params = Object.keys(result).map(key => ({ ...result[key], key}));
+        // 5.2. price順にソートして差分更新完了
+        //        result.items.sort((a, b) => b.percentage - a.percentage);
+        // 6.   Function化によるサイト単位の処理になった場合に難易度アップ？Function化も併せて要検討
+
+        // Function化
+        // 優先事項としては、将来的に規模が大きくなってもロジックの大きな修正不要、コストは二の次でOK。
+        // ・ローカル同様にアイテム単位で取得
+        //   △単純に現状の並列実行だとメモリが足りない都合、並列数を制限が前提。シングルプロセスのためクロールサイトが多くなるとつらくなってくる。
+        //   △待ち時間のコストは無駄が多そう。
+        //   〇ローカルプログラムと共有部分多い。
+        // ・サイトごとにFunctionを割り当てる
+        //   △サイトへのアクセス間隔の管理が単純。一方で待ち時間のコストは無駄になる。
+        //   △ローカルプログラムとは作りが大きく変わる可能性あり。
+        // ・サイトごと更にアイテムごとにFunctionを割り当てる
+        //   △サイトへのアクセス間隔の管理が複雑（1アイテムごとに次のキューを投げる？）。一方で待ち時間のコストは最適化される。(ブラウザ起動のオーバーヘッドは要検証)
+        //   △ローカルプログラムとは作りが大きく変わる可能性あり。
+
 
         await crawlMain(browser, items, result, resultFailed);
         // for dev
